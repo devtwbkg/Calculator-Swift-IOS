2217d321b7aae15691aa05278b40262d3d854ec0
Wittaya <wittaya.b@lansingbs.com>
2019-05-07 16:03:52 +0700

    add view

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var resultValue : Double = 0.0
+    var numberArray = [Double]()
+    var currentInput : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resultLabel.text = "0"
     }
+ 
 
-
+    @IBAction func onNumberButtonPressed(_ sender: UIButton) {
+        print("tag is ", sender.tag)
+    }
+    
+    @IBAction func onClearButtonPressed(_ sender: UIButton) {
+        print("onClearButtonPressed")
+        
+    }
+    
+    @IBAction func onAddOrMinButtonPressed(_ sender: UIButton) {
+        print("onAddOrMinButtonPressed")
+    }
+    
+    @IBAction func onModButtonPressed(_ sender: UIButton) {
+        print("onModButtonPressed")
+    }
+    
+    @IBAction func onDivButtonPressed(_ sender: UIButton) {
+        print("onDivButtonPressed")
+    }
+    
+    @IBAction func onMulButtonPressed(_ sender: UIButton) {
+        print("onMulButtonPressed")
+    }
+    
+    @IBAction func onMinButtonPressed(_ sender: UIButton) {
+        print("onMinButtonPressed")
+    }
+    
+    @IBAction func onAddButtonPressed(_ sender: UIButton) {
+        print("onAddButtonPrerssed")
+    }
+    
+    @IBAction func onEqualButtonPressed(_ sender: UIButton) {
+        print("onEqualButotnPressed")
+    }
+    
+    @IBAction func onDotButtonPressed(_ sender: UIButton) {
+        print("onDotButtonPressed")
+    }
+    
+    
 }
 
